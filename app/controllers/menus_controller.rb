8b5b34b90ac4e343c9fb7cdb8a336de3d5f005ea
Thiago Costa <thiagohrcosta86@gmail.com>
2021-04-22 23:36:10 -0300

    Adding actions to Controller and create index view

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -2,4 +2,27 @@ class MenusController < ApplicationController
   def index
     @menus = Menu.all
   end
+
+  def show
+    @menu = Menu.find(params[:id])
+  end
+
+  def new
+    @menu = Menu.new
+  end
+
+  def create
+    @menu = Menu.new(menu_params)
+    if @menu.save
+      redirect_to menu_path(@menu)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def menu_params
+    params.require(:menu).permit(:name, :description, :menu, :picture, :price)
+  end
 end
